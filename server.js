6fc9cb1237852b61f6a29f8fd792b99758b21c8c
TrevorTress <trevor.tress@outlook.com>
2022-05-26 19:06:37 -0400

    updated user routes

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
+// TO-DO: Add handlebars, finalize routes, code site logic in public js, add css in public css
+
 // WHEN I visit the site for the first time
 // THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
 
